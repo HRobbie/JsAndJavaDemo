d7214af05b2eb90a49ff8ffabc07685a031aacb6
HRobbie <hwwyouxiang@163.com>
2016-10-28 14:24:27 +0800

    增加进入的时候加载js

--- js/src/main/assets/js/index.js
+++ js/src/main/assets/js/index.js
@@ -1,6 +1,13 @@
+/**
+外层不能是$(fucntion(){})
+**/
+
+
 function javaCallJs(arg){
 	$('#content').html("<br/>"+arg);
 }
+
+
 function showHtmlcallJava(){
 	var str=window.Android.returnSomething("我");
 	alert(str);
@@ -10,4 +17,8 @@ function javaTojava(arg){
     $('#content').html("<br/>"+arg);
     var str1="234";
     javascript:Android.toastSomething(str1);
+}
+
+function stop(arg){
+    $('#stop').html("<br/>"+arg);
 }
--- js/src/main/java/com/towatt/charge/js/MainActivity.java
+++ js/src/main/java/com/towatt/charge/js/MainActivity.java
@@ -1,7 +1,8 @@
 package com.towatt.charge.js;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
@@ -14,6 +15,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private WebView web_view;
     private ProgressBar pb_loading;
+    String load="1234565";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         web_view = (WebView)findViewById(R.id.web_view);
         pb_loading = (ProgressBar)findViewById(R.id.pb_loading);
         web_view.loadUrl("file:///android_asset/index1.html");
+//        web_view.loadUrl("http://192.168.1.80:8020/websocket/index.html");
+
 
         //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
         web_view.setWebViewClient(new WebViewClient(){
@@ -32,12 +36,22 @@ public class MainActivity extends AppCompatActivity {
                 view.loadUrl(url);
                 return true;
             }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                //当页面加载成功之后，才能执行这个。
+//                web_view.loadUrl("javascript:javaCallJs("+load+")");
+//                web_view.loadUrl("javascript:connectToWebSocket('14')");
+//                Log.e("TAG", "load="+load);
+            }
         });
 
         //启用支持javascript
         WebSettings settings = web_view.getSettings();
         settings.setJavaScriptEnabled(true);
-        web_view.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        //不加载缓存
+        web_view.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
 
 
         web_view.setWebChromeClient(new WebChromeClient() {
@@ -53,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             }
+
+
         });
         settings.setDefaultTextEncodingName("utf-8");
         settings.setUseWideViewPort(true);//设定支持viewport
@@ -68,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
         //注意Java调用JS的时候addJavascriptInterface是不必须的
 
         web_view.addJavascriptInterface(new Call(), "Android");
+
     }
 
     //改写物理按键——返回的逻辑
@@ -91,16 +108,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void clock1(View v) {
         web_view.loadUrl("javascript:javaCallJs('我')");
+
+//        web_view.loadUrl("javascript:webSocketStop()");
     }
 
     public void clock2(View v) {
         web_view.loadUrl("javascript:javaTojava('你')");
+////        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+////        startActivity(intent);
+//        web_view.loadUrl("javascript:connectToWebSocket('14')");
     }
 
     public class Call{
         @android.webkit.JavascriptInterface
         public void showHtmlcallJava(){
             Toast.makeText(MainActivity.this,"123",Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
         }
         @android.webkit.JavascriptInterface
         public void toastSomething(String str){
@@ -110,6 +135,27 @@ public class MainActivity extends AppCompatActivity {
         public String returnSomething(String str){
             return str+":"+"123";
         }
+
+
+        @android.webkit.JavascriptInterface
+        public void toConsumptionActivity(){
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //这个方法可以放到stop里面执行
+        web_view.loadUrl("javascript:stop('stop')");
+    }
+
+    @Override
+    protected void onDestroy() {
+//        web_view.loadUrl("javascript:stop()");
+        super.onDestroy();
+
     }
 
 
